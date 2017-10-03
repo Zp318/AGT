638a0435e1994d8c42afd2e6210128357da2672f
zhangp <zp421531349@163.com>
2017-10-03 18:42:08 +0800

    modify

--- src/config/urls.js
+++ src/config/urls.js
@@ -1,15 +1,28 @@
 
+// var url = {
+// 	leftTopSelect:window.basePath + "multiAnalysis/indexDataQuery.action",
+// 	leftTopChartLeft : window.basePath + "multiAnalysis/kqiDataQuery.action",
+// 	leftTopChartRight :window.basePath + "multiAnalysis/subKqiDataQuery.action",
+// 	leftBottomNav:window.basePath + "multiAnalysis/getDimensionValues.action",
+// 	leftBottomChart:window.basePath + "multiAnalysis/filterDataQuery.action",
+// 	rightGrid: window.basePath + "multiAnalysis/detailDataQuery.action",
+// 	exportGridData: window.basePath + "multiAnalysis/detailDataExport.action",
+// 	initFilterData: window.basePath + "multiAnalysis/initFilterData.action",
+// 	maskChart: window.basePath + "multiAnalysis/filterDataTendencyQuery.action",
+// 	rejectDataQuery:window.basePath + "multiAnalysis/rejectDataQuery.action",
+// 	detailsSheet:window.seqBasePath + "sdpi/pages/dashboard/drillDetail.jsp?params="
+// }
 var url = {
-	leftTopSelect:window.basePath + "multiAnalysis/indexDataQuery.action",
-	leftTopChartLeft : window.basePath + "multiAnalysis/kqiDataQuery.action",
-	leftTopChartRight :window.basePath + "multiAnalysis/subKqiDataQuery.action",
-	leftBottomNav:window.basePath + "multiAnalysis/getDimensionValues.action",
-	leftBottomChart:window.basePath + "multiAnalysis/filterDataQuery.action",
-	rightGrid: window.basePath + "multiAnalysis/detailDataQuery.action",
-	exportGridData: window.basePath + "multiAnalysis/detailDataExport.action",
-	initFilterData: window.basePath + "multiAnalysis/initFilterData.action",
-	maskChart: window.basePath + "multiAnalysis/filterDataTendencyQuery.action",
-	rejectDataQuery:window.basePath + "multiAnalysis/rejectDataQuery.action",
-	detailsSheet:window.seqBasePath + "sdpi/pages/dashboard/drillDetail.jsp?params="
+	leftTopSelect:"multiAnalysis/indexDataQuery.action",
+	leftTopChartLeft : "multiAnalysis/kqiDataQuery.action",
+	leftTopChartRight :"multiAnalysis/subKqiDataQuery.action",
+	leftBottomNav:"multiAnalysis/getDimensionValues.action",
+	leftBottomChart:"multiAnalysis/filterDataQuery.action",
+	rightGrid: "multiAnalysis/detailDataQuery.action",
+	exportGridData: "multiAnalysis/detailDataExport.action",
+	initFilterData: "multiAnalysis/initFilterData.action",
+	maskChart: "multiAnalysis/filterDataTendencyQuery.action",
+	rejectDataQuery:"multiAnalysis/rejectDataQuery.action",
+	detailsSheet:"sdpi/pages/dashboard/drillDetail.jsp?params="
 }
 export default url
